7fe3626bb74b33c391085a5335274e1b7393ddd9
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-04-26 14:05:56 +0000

    01 Default Spring Init Code

---
+++ README.md
@@ -0,0 +1,4 @@
+### Refrence Tutorials
+
+
+https://www.youtube.com/playlist?list=PL1oBBulPlvs84AmRmT-_3dGz4KHYuINsj
---
+++ src/test/java/com/bibf/api/employeebackend/EmployeebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibf.api.employeebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeebackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
