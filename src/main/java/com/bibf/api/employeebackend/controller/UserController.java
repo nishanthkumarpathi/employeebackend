d4ace7243a0f400e9882713fbca69298996852de
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-04-27 13:06:26 +0000

    03 @PostMapping for sending data in database

---
+++ src/main/java/com/bibf/api/employeebackend/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.bibf.api.employeebackend.controller;
+
+import com.bibf.api.employeebackend.model.User;
+import com.bibf.api.employeebackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+}
