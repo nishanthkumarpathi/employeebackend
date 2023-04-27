eeaa04d53e9be0642b940dcd9c88504ea2649a69
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-04-27 16:46:14 +0000

    09-04 enabled CORS, so that frontend can talk

--- src/main/java/com/bibf/api/employeebackend/controller/UserController.java
+++ src/main/java/com/bibf/api/employeebackend/controller/UserController.java
@@ -3,14 +3,12 @@ package com.bibf.api.employeebackend.controller;
 import com.bibf.api.employeebackend.model.User;
 import com.bibf.api.employeebackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*") //WildCard is not good
 public class UserController {
 
     private final UserRepository userRepository;
